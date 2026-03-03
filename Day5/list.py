91cebd8567d6f80feb6766b7fd962beb87f91d96
vagi5314 <vagi5314@users.noreply.github.com>
2026-03-03 23:56:01 +0530

    day 5 - started learning lists and their methods

---
+++ Day5/list.py
@@ -0,0 +1,36 @@
+#list and its methods
+list1 = [56, 76, 89, 75, 13, 12, 90, 45]
+list2 = [34, 67, 59, 43, 38, 93, 27, 73]
+
+print(len(list1+list2))
+list3=list1.extend(list2)
+print(list3)
+
+#append and remove
+list1.append(100)
+print(list1)
+list1.remove(56)
+print(list1)
+
+#sort and reverse
+list2.sort()
+print(list2)
+list2.reverse()
+print(list2)
+
+#indexing and slicing
+print(list2[0])
+print(list2[-1])
+print(list2[2:5])
+
+
+print(min(list=list2))
+print(max(list1+list2))
+
+print(list1.count(56))
+
+print(list1.index(56))
+
+list1.insert(2, 100)
+print(list1)
+
