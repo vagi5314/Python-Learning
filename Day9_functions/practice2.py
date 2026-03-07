d7e3451dde1cc40a5ab27cfeafb77fc5ee0039b0
vagi5314 <vagi5314@users.noreply.github.com>
2026-03-07 23:57:36 +0530

    wrote a function to check odd or even numbers

---
+++ Day9_functions/practice2.py
@@ -0,0 +1,8 @@
+#function for even orodd
+def oddeven():
+    num = int(input("enter the value of num:"))
+    if num%2!=0:
+        print(num,"is odd")
+    else:
+        print(num,"is even")
+oddeven()
