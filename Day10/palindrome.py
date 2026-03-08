4b548dca843f78707fda47fbbe3a054291b33a42
vagi5314 <vagi5314@users.noreply.github.com>
2026-03-08 23:58:09 +0530

    day 10 - checked for palindromes

---
+++ Day10/palindrome.py
@@ -0,0 +1,19 @@
+#way1
+word = input("enter the word")
+rev = ""
+for ch in word:
+    rev = ch + rev
+
+if word == rev:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+
+#Another way to do it
+word = input("enter the word")
+rev=word[::-1]
+if word==rev:
+    print("palindrome")
+else:
+    print("Not a palindrome")
+
