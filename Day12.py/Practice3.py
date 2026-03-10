66a9f34fdfce88db48bd9bee9e353b706f1a8d0f
vagi5314 <vagi5314@users.noreply.github.com>
2026-03-10 23:34:04 +0530

    finding the largest of three numbers

---
+++ Day12.py/Practice3.py
@@ -0,0 +1,12 @@
+#largest number
+def largest(a,b,c):
+    if a>b and a>c:
+        return a,"is largest"
+    elif b>a and b>c:
+        return b,"is largest"
+    else:
+        return c,"is largest"
+a = int(input("enter the value of a : "))
+b = int(input("enter the value of b : "))
+c = int(input("enter the value of c : "))
+print(largest(a,b,c))
