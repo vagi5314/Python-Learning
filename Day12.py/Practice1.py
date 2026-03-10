c950a11d012ddfcbebea481ff72106a104dd9ac9
vagi5314 <vagi5314@users.noreply.github.com>
2026-03-10 23:33:35 +0530

    day 12 - area of rectangle using functions

---
+++ Day12.py/Practice1.py
@@ -0,0 +1,6 @@
+def area_of_rectangle(l, b):
+    return l*b
+length = int(input("enter the length"))
+breadth = int(input("enter the breadth"))
+result = area_of_rectangle(length, breadth)
+print(f"The area of rectangle is {result}")
