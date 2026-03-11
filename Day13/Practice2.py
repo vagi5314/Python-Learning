0d9203341e56fa133293826c815753542bd99b86
vagi5314 <vagi5314@users.noreply.github.com>
2026-03-11 22:22:52 +0530

    palindrome checker as a proper function

---
+++ Day13/Practice2.py
@@ -0,0 +1,14 @@
+#palindrome function
+def palindrome(word):
+    rev = ""
+    for i in word:
+        rev = i+rev
+    if(word == rev):
+        print("palindrome")
+    else:
+        print("not a palindrome")
+    return rev
+
+new = palindrome(word = input("enter the word : "))
+print(new)
+
