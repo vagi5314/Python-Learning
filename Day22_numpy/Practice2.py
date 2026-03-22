69c4e2cdb9559eb0e44254f2b1117a11cfae9b4e
vagi5314 <vagi5314@users.noreply.github.com>
2026-03-22 23:22:11 +0530

    numpy statistical functions - sum mean median variance and std

---
+++ Day22_numpy/Practice2.py
@@ -0,0 +1,10 @@
+import numpy as np
+array = np.array([1,7,9,3])
+print(np.sum(array))
+print(np.mean(array))
+print(np.median(array))
+print(np.max(array))
+print(np.min(array))
+print(np.var(array))
+print(np.std(array))
+
