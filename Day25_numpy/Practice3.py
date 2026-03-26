a54535787468f81aa7f6fcc901854c2cfdc52830
vagi5314 <vagi5314@users.noreply.github.com>
2026-03-26 22:20:31 +0530

    day 25 - exploring array-scalar and array-array arithmetic operations

---
+++ Day25_numpy/Practice3.py
@@ -0,0 +1,14 @@
+#with2d
+import numpy as np
+arr = np.array([1,2,7,9,11])
+brr = np.array([11,6,7,9,10])
+print(arr-brr)
+print(arr+brr)
+print(arr*brr)
+print(arr/brr)
+print(arr**brr)
+print(arr%brr)
+print(arr>brr)
+print(arr<brr)
+print(arr==brr)
+print(arr!=brr)
---
+++ Day25_numpy/practice2.py
@@ -0,0 +1,14 @@
+#arithmetic operations
+import numpy as np
+arr = np.array([23,67,89,12,34])
+print(arr)
+print(arr+1)
+print(arr-1)
+print(arr*2)
+print(arr/2)
+print(arr**2)
+print(arr%2)
+print(arr>2)
+print(arr<2)
+print(arr==2)
+print(arr!=2)
