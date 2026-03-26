73f770a51530ba1ffd2c54f3691ed8489d2385b1
vagi5314 <vagi5314@users.noreply.github.com>
2026-03-26 22:21:58 +0530

    day 25 - utilizing generation functions and analyzing statistical measures

---
+++ Day25_numpy/Practice5.py
@@ -0,0 +1,9 @@
+#function and aggregation
+import numpy as np
+print(np.zeros((2,2,3), dtype=int))
+print(np.ones((3,2,3), dtype=int))
+print(np.full((2,2,3), 5, dtype=int))
+print(np.eye(3, dtype=int))
+print(np.arange(1, 102, 2))
+print(np.linspace(1, 100, 4))
+
---
+++ Day25_numpy/Practice6.py
@@ -0,0 +1,12 @@
+import numpy as np
+arr = np.array([[1,2,3],[43,1,7]])
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.std(arr))
+print(np.var(arr))
+print(np.min(arr))
+print(np.max(arr))
+print(np.argmin(arr))
+print(np.argmax(arr))
+print(np.sqrt(arr))
+
